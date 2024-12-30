91d1b6e055554c6641157312ef55a42516862965
Le Duc Nam <arthit@Joong.local>
2024-12-30 16:29:07 +0700

    Refactor | Init project with react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'primereact/button';  
+function App() {
+
+  return (
+    <>
+     <Button label="Submit" className='mt-6 block bg-red-500'/>
+    </>
+  );
+}
+
+export default App;
--- src/lib/helperApi.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-
-import { cookies } from 'next/headers'
-
-export const getCookies = async (key: string) => {
-  const cookiesStore = cookies();
-  const accessToken = cookiesStore.get(key);
-  return accessToken?.value
-}
